e9b6750fa61a25ad051ef6138e2c3aa7891a684a
chichong <k1997102454@gmail.com>
2019-12-01 22:44:31 +0800

    trigger on

--- app.py
+++ app.py
@@ -38,7 +38,11 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(machine.state))
+    try:
+        machine.trigger('message')
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(machine.state))
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage("no trigger found"))
     
 
 if __name__ == "__main__":
